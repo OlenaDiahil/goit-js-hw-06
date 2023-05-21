b5ff0e388bef8f5a1dabec10d65b86da2d868b44
OlenaDiahil <122643832+OlenaDiahil@users.noreply.github.com>
2023-05-21 11:28:33 +0200

    upp

--- js/task-10.js
+++ js/task-10.js
@@ -4,35 +4,3 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const controls = document.getElementById('controls');
-const createButton = controls.querySelector('[data-create]');
-const destroyButton = controls.querySelector('[data-destroy]');
-const boxesContainer = document.getElementById('boxes');
-
-createButton.addEventListener('click', function() {
-  const amount = Number(document.querySelector('#controls input').value);
-  createBoxes(amount);
-});
-
-destroyButton.addEventListener('click', destroyBoxes);
-
-function createBoxes(amount) {
-  const boxes = [];
-
-  for (let i = 0; i < amount; i++) {
-    const size = 30 + i * 10;
-    const color = getRandomHexColor();
-    const box = document.createElement('div');
-    box.classList.add('box');
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
-    box.style.backgroundColor = color;
-    boxes.push(box);
-  }
-
-  boxesContainer.append(...boxes);
-}
-
-function destroyBoxes() {
-  boxesContainer.innerHTML = '';
-}
